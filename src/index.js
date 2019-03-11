21c08e97d075fc656cfb56ac9a0dd3556f57b356
Juan <oqundo.juan@system-in-motion.com>
2019-03-11 15:38:10 +0800

    adding socket.io and complete api

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import http from 'http';
 import express from 'express';
 import bodyParser from 'body-parser';
 import passport from 'passport';
+import socket from 'socket.io';
+import Message from './model/message';
+import Channel from './model/channel';
 
 import config from './config';
 import routes from './routes';
@@ -11,6 +14,8 @@ const LocalStrategy  = require('passport-local').Strategy;
 
 const app = express();
 app.server = http.createServer(app);
+let io = socket(app.server);
+
 
 app.use(bodyParser.json({
     limit:'100kb'
@@ -35,5 +40,70 @@ app.use('/',(req,res)=>{
     res.json('this chat is alive!')
 });
 
+/*||||||||||||||||SOCKET|||||||||||||||||||||||*/
+//Listen for connection
+var typingUsers = {};
+
+io.on('connection', function(client) {
+  console.log('a user connected');
+  //Listens for a new chat message
+  client.on('newChannel', function(name, description) {
+    //Create channel
+    let newChannel = new Channel({
+    name: name,
+    description: description,
+  });
+    //Save it to database
+    newChannel.save(function(err, channel){
+      //Send message to those connected in the room
+      console.log('new channel created');
+      io.emit("channelCreated", channel.name, channel.description, channel.id);
+    });
+  });
+
+  //Listens for user typing.
+  client.on("startType", function(userName, channelId){
+    console.log("User " + userName + " is writing a message...");
+    typingUsers[userName] = channelId;
+    io.emit("userTypingUpdate", typingUsers, channelId);
+  });
+
+  client.on("stopType", function(userName){
+    console.log("User " + userName + " has stopped writing a message...");
+    delete typingUsers[userName];
+    io.emit("userTypingUpdate", typingUsers);
+  });
+
+  //Listens for a new chat message
+  client.on('newMessage', function(messageBody, userId, channelId, userName, userAvatar, userAvatarColor) {
+    //Create message
+
+    console.log(messageBody);
+
+    let newMessage = new Message({
+    messageBody: messageBody,
+    userId: userId,
+    channelId: channelId,
+    userName: userName,
+    userAvatar: userAvatar,
+    userAvatarColor: userAvatarColor
+  });
+    //Save it to database
+    newMessage.save(function(err, msg){
+      //Send message to those connected in the room
+      console.log('new message sent');
+
+      io.emit("messageCreated",  msg.messageBody, msg.userId, msg.channelId, msg.userName, msg.userAvatar, msg.userAvatarColor, msg.id, msg.timeStamp);
+    });
+  });
+});
+/*||||||||||||||||||||END SOCKETS||||||||||||||||||*/
+
 app.server.listen(config.port);
 console.log(`Server started at port ${app.server.address().port}`);
+
+
+module.exports = {
+  app,
+  io
+}
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,8 @@ import express from 'express';
 import initializeDb from '../db';
 import user from '../controller/user';
 import account from '../controller/account';
+import channel from '../controller/channel';
+import message from '../controller/message';
 import config from '../config';
 import middleware from '../middleware';
 
@@ -11,8 +13,11 @@ initializeDb(db =>{
      //internal middleware
     router.use(middleware({ config, db }));
 
-    router.use('/user',user({config,db}));
+     //api routes v1 (/v1)
+    router.use('/user', user({ config, db }));
     router.use('/account', account({ config, db }));
+    router.use('/channel', channel({ config, db }));
+    router.use('/message', message({ config, db }));
 
 })
 
